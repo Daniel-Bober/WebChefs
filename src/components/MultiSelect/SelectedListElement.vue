602d589de5b210da1e40ecdb13125ff646beb5c4
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-02-12 20:31:13 +0100

    feat: init SelectedListElement comp

---
+++ src/components/MultiSelect/SelectedListElement.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="selected-list-element">
+  <div class="title">{{ props.title }}</div>
+  <button @click="removeFromSelected"><img src="src/assets/images/icons/x.svg" alt="delete icon"></button>
+</div>
+</template>
+
+<script setup lang='ts'>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: Number,
+    required: true
+  }
+});
+
+const emit = defineEmits(["removeFromSelected"])
+
+function removeFromSelected() {
+  emit("removeFromSelected", props.title)
+}
+</script>
+
+<style lang='scss' scoped>
+.selected-list-element {
+  max-height: 22px;
+  padding: 6px 0 6px 8px;
+  display: flex;
+  align-items: center;
+  background-color: $ui-g6;
+  border-radius: 2px;
+  user-select: none;
+
+  .title {
+    font-size: $font-11px;
+    color: $ui-g1;
+  }
+
+  button {
+    width: 24px;
+    height: 24px;
+    margin-left: 6px;
+  }
+}
+</style>
